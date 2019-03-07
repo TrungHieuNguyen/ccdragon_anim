cbc393ab7fc807ca792acc0d37dd1b2629748b1b
MrChip <mrchip@hotmail.com>
2019-03-07 11:18:28 +0700

    add game

--- project/Classes/AppDelegate.cpp
+++ project/Classes/AppDelegate.cpp
@@ -1,141 +1,93 @@
-/****************************************************************************
- Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
- 
- http://www.cocos2d-x.org
- 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
- 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- ****************************************************************************/
-
 #include "AppDelegate.h"
-#include "GameScene.h"
-#include "SlotTester.hpp"
-//#include "CoreElement.h"
-// #define USE_AUDIO_ENGINE 1
-// #define USE_SIMPLE_AUDIO_ENGINE 1
 
-#if USE_AUDIO_ENGINE && USE_SIMPLE_AUDIO_ENGINE
-#error "Don't use AudioEngine and SimpleAudioEngine at the same time. Please just select one in your game!"
-#endif
-
-#if USE_AUDIO_ENGINE
-#include "audio/include/AudioEngine.h"
-using namespace cocos2d::experimental;
-#elif USE_SIMPLE_AUDIO_ENGINE
-#include "audio/include/SimpleAudioEngine.h"
-using namespace CocosDenshion;
-#endif
+#include "demo/HelloDragonBones.h"
+#include "demo/AnimationBase.h"
+#include "demo/DragonBonesEvent.h"
+#include "demo/AnimationLayer.h"
+#include "demo/BoneOffset.h"
+#include "demo/InverseKinematics.h"
+#include "demo/BoundingBox.h"
+#include "demo/ReplaceSlotDisplay.h"
+#include "demo/ReplaceSkin.h"
+#include "demo/ReplaceAnimation.h"
+#include "demo/CoreElement.h"
+#include "demo/PerformanceTest.h"
+// #include "DragonBonesTest.h"
 
 USING_NS_CC;
 
-//static cocos2d::Size designResolutionSize = cocos2d::Size(800, 480);
-//static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
-//static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
-//static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
-
-static cocos2d::Size designResolutionSize = cocos2d::Size(480, 800);
-
-
-AppDelegate::AppDelegate()
+AppDelegate::AppDelegate() 
 {
 }
 
 AppDelegate::~AppDelegate() 
 {
-#if USE_AUDIO_ENGINE
-    AudioEngine::end();
-#elif USE_SIMPLE_AUDIO_ENGINE
-    SimpleAudioEngine::end();
-#endif
 }
 
+//if you want a different context,just modify the value of glContextAttrs
+//it will takes effect on all platforms
 void AppDelegate::initGLContextAttrs()
 {
-    GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8, 0};
+    //set OpenGL context attributions,now can only set six attributions:
+    //red,green,blue,alpha,depth,stencil
+    GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8};
 
     GLView::setGLContextAttrs(glContextAttrs);
 }
 
-static int register_all_packages()
-{
-    return 0;
-}
-
 bool AppDelegate::applicationDidFinishLaunching() {
-
+    // initialize director
     auto director = Director::getInstance();
     auto glview = director->getOpenGLView();
     if(!glview) {
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
-        glview = GLViewImpl::createWithRect("Game2D", cocos2d::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
-#else
-        glview = GLViewImpl::create("Game2D");
-#endif
+        glview = GLViewImpl::createWithRect("DragonBones", cocos2d::Rect(0, 0, 1136, 640));
         director->setOpenGLView(glview);
     }
 
-    director->setDisplayStats(false);
-    director->setAnimationInterval(1.0f / 60);
-    glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, ResolutionPolicy::NO_BORDER);
-    auto frameSize = glview->getFrameSize();
-//    if (frameSize.height > mediumResolutionSize.height)
-//    {        
-//        director->setContentScaleFactor(MIN(largeResolutionSize.height/designResolutionSize.height, largeResolutionSize.width/designResolutionSize.width));
-//    }
-//    else if (frameSize.height > smallResolutionSize.height)
-//    {        
-//        director->setContentScaleFactor(MIN(mediumResolutionSize.height/designResolutionSize.height, mediumResolutionSize.width/designResolutionSize.width));
-//    }
-//    else
-//    {        
-//        director->setContentScaleFactor(MIN(smallResolutionSize.height/designResolutionSize.height, smallResolutionSize.width/designResolutionSize.width));
-//    }
-    FileUtils::getInstance()->addSearchPath("res");
-    register_all_packages();
-    //auto scene = GameScene::createScene();
-    auto scene = SlotTester::createScene();
-    //auto scene = CoreElementGame::createScene();
+    director->getOpenGLView()->setDesignResolutionSize(1136, 640, ResolutionPolicy::SHOW_ALL);
 
+    // turn on display FPS
+    director->setDisplayStats(true);
+
+    // set FPS. the default value is 1.0/60 if you don't call this
+    director->setAnimationInterval(1.0f / 60.f);
+    FileUtils::getInstance()->addSearchPath("assets");
+    FileUtils::getInstance()->addSearchPath("res");
+
+    // create a scene. it's an autorelease object
+    auto scene = HelloDragonBones::createScene();
+    // auto scene = AnimationBase::createScene();
+    // auto scene = DragonBonesEvent::createScene();
+    // auto scene = AnimationLayer::createScene();
+    // auto scene = BoneOffset::createScene();
+    // auto scene = InverseKinematics::createScene();
+    // auto scene = BoundingBox::createScene();
+    // auto scene = ReplaceSlotDisplay::createScene();
+    // auto scene = ReplaceSkin::createScene();
+    // auto scene = ReplaceAnimation::createScene();
+    // auto scene = CoreElementGame::createScene();
+    // auto scene = PerformanceTest::createScene();
+
+    // auto scene = DragonBonesTest::createScene();
+
+    // run
     director->runWithScene(scene);
 
     return true;
 }
 
-// This function will be called when the app is inactive. Note, when receiving a phone call it is invoked.
+// This function will be called when the app is inactive. When comes a phone call,it's be invoked too
 void AppDelegate::applicationDidEnterBackground() {
     Director::getInstance()->stopAnimation();
 
-#if USE_AUDIO_ENGINE
-    AudioEngine::pauseAll();
-#elif USE_SIMPLE_AUDIO_ENGINE
-    SimpleAudioEngine::getInstance()->pauseBackgroundMusic();
-    SimpleAudioEngine::getInstance()->pauseAllEffects();
-#endif
+    // if you use SimpleAudioEngine, it must be pause
+    // SimpleAudioEngine::getInstance()->pauseBackgroundMusic();
 }
 
 // this function will be called when the app is active again
 void AppDelegate::applicationWillEnterForeground() {
     Director::getInstance()->startAnimation();
 
-#if USE_AUDIO_ENGINE
-    AudioEngine::resumeAll();
-#elif USE_SIMPLE_AUDIO_ENGINE
-    SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
-    SimpleAudioEngine::getInstance()->resumeAllEffects();
-#endif
+    // if you use SimpleAudioEngine, it must resume here
+    // SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
 }
